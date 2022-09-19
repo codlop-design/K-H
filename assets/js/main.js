1bfb4a884e28268709f0c429443886efe61bfcb0
abdohantash <abdohantash9999@gmail.com>
2022-09-19 14:44:56 +0200

    k-h2

--- assets/js/main.js
+++ assets/js/main.js
@@ -322,54 +322,6 @@ jQuery(function ($) {
 		}
 	});
 
-	// Subscribe form
-	$(".newsletter-form").validator().on("submit", function (event) {
-		if (event.isDefaultPrevented()) {
-		// handle the invalid form...
-			formErrorSub();
-			submitMSGSub(false, "Please enter your email correctly.");
-		} 
-		else {
-			// everything looks good!
-			event.preventDefault();
-		}
-	});
-	function callbackFunction (resp) {
-		if (resp.result === "success") {
-			formSuccessSub();
-		}
-		else {
-			formErrorSub();
-		}
-	}
-	function formSuccessSub(){
-		$(".newsletter-form")[0].reset();
-		submitMSGSub(true, "Thank you for subscribing!");
-		setTimeout(function() {
-			$("#validator-newsletter").addClass('hide');
-		}, 4000)
-	}
-	function formErrorSub(){
-		$(".newsletter-form").addClass("animated shake");
-		setTimeout(function() {
-			$(".newsletter-form").removeClass("animated shake");
-		}, 1000)
-	}
-	function submitMSGSub(valid, msg){
-		if(valid){
-			var msgClasses = "validation-success";
-		} 
-		else {
-			var msgClasses = "validation-danger";
-		}
-		$("#validator-newsletter").removeClass().addClass(msgClasses).text(msg);
-	}
-	// AJAX MailChimp
-	$(".newsletter-form").ajaxChimp({
-		url: "https://envytheme.us20.list-manage.com/subscribe/post?u=60e1ffe2e8a68ce1204cd39a5&amp;id=42d6d188d9", // Your url MailChimp
-		callback: callbackFunction
-	});
-
 	// Count Time 
 	function makeTimer() {
 		var endTime = new Date("September 20, 2025 17:00:00 PDT");			
@@ -499,12 +451,16 @@ jQuery(function ($) {
 		jQuery(".preloader").fadeOut(500);
 	});
 
+	$('.filter-cards').mixItUp();
+
+	
+
+
 	// // Buy Now Btn
 	// $('body').append("<a href='https://themeforest.net/checkout/from_item/35190746?license=regular&support=bundle_6month&_ga=2.74264149.634514020.1646539215-1425290503.1590986634' target='_blank' class='buy-now-btn'>Buy Now</a>");
 
 	// // Switch Btn
 	// $('body').append("<div class='switch-box'><label id='switch' class='switch'><input type='checkbox' onchange='toggleTheme()' id='slider'><span class='slider round'></span></label></div>");
-
 }(jQuery));
 
 // function setTheme(themeName) {
